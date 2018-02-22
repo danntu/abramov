cf2b6e2368e623b53e37f377571f2bb364bf9bab
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-02-22 21:56:33 +0600

    Task1

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Vvedite a->");
+        int a = sc.nextInt();
+
+        System.out.print("Vvedite b->");
+        int b = sc.nextInt();
+
+        System.out.println("Summa a+b = "+(a+b));
+        System.out.println("Raznost a-b = "+(a-b));
+        System.out.println("Proizvedenie a*b = "+(a*b));
+    }
 }
